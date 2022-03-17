472af4b40e21d0ac4e3d736ce29228138b1bdfff
HP <mellaniabks.gmail.com>
2022-03-17 12:24:58 +0700

    tugas

--- src/main/kotlin/MainApp.kt
+++ src/main/kotlin/MainApp.kt
@@ -1,3 +1,6 @@
+//fun adalah sebuah keyword yang menandakan sebuah fungsi dikotlin
+//fun main() adalah fungsi main diaplikasi Kotlin dimana sebagai titik awal ketika program dijalankan.
 fun main(args: Array<String>){
     println("Hello, ini program pertama saya dikotlin!")
+    //println() adalah fungsi dasar untuk mencetak string ke layar.
 }
