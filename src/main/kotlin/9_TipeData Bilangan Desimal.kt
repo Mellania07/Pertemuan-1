4d83b89e1bbff3530c46a5c19950df8ab33dc7f0
HP <mellaniabks.gmail.com>
2022-03-24 06:37:10 +0700

    contoh Tipe Data Bilangan Desimal

---
+++ src/main/kotlin/9_TipeData Bilangan Desimal.kt
@@ -0,0 +1,12 @@
+fun main(args: Array<String>) {
+    //tipe data Float
+    //variabel maxFloat yaitu hanya 6 digit(kelebihan digit akan dibulatkan)
+    // Untuk memberikan nilai sebagai float maka harus memberikan akhiran f atau F pada akhir literal.
+    val maxFloat: Float = 9.123456789f
+    //tipe data double
+    //variabel maxDouble yaitu mencapai 10 digit
+    val maxDouble: Double = 9.123456789
+
+    println("maxFloat:" + maxFloat)
+    println("maxDouble:" + maxDouble)
+}
