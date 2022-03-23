95917d183eb8eab2698ce6fd69f3940130da5163
HP <mellaniabks.gmail.com>
2022-03-24 06:48:54 +0700

    contoh Constants Literal

--- src/main/kotlin/10_Constants Literal.kt
+++ src/main/kotlin/10_Constants Literal.kt
@@ -1,3 +1,4 @@
+//underscore dalam literal, untuk memberikan kemudahan pada pengembang dalam membaca dan menulis kode program
 fun main(args: Array<String>) {
     val floatLiteral : Float = 178.95F //Literal Float
     println("Contoh Literal Float : " + floatLiteral)
