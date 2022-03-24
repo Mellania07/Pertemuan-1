165698e0fadd411eea77be076b3ab07f3417a395
HP <mellaniabks.gmail.com>
2022-03-24 10:03:28 +0700

    contoh String Operator

---
+++ src/main/kotlin/18_String operator.kt
@@ -0,0 +1,6 @@
+//menggabungkan String dengan yang lain dengan menggunakan operator +
+//sehingga tipe data datang digabung otomatis kedalam string
+fun main(args: Array<String>) {
+    val s = "abc" + 1
+    println(s + "def")
+}
