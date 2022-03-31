af5f653f8dc6c2539e3fc0ec862c39f1f63179f0
HP <mellaniabks.gmail.com>
2022-03-31 09:33:27 +0700

    Perintah masukkan di kotlin

---
+++ src/main/kotlin/26_Parrot program.kt
@@ -0,0 +1,12 @@
+//sebuah fungsi readLine()!!. Secara default perintah masukkan dikotlin akan
+// mengembalikan nilai string ke dalam variabel penampung
+fun main(args: Array<String>) {
+    //parrot program
+    println("Hi I'm Lora, the virtual parrot and i love to repeat");
+    println("Type something in:");
+    var input: String
+    input = readLine()!!
+    var output: String
+    output = input + ", " + input + "!"
+    println(output)
+}
